4bf1f258b1db758ebad592bd2dd15eeeb513943f
Socratic1 <samuel.rodney.phillips@gmail.com>
2016-09-06 13:44:14 +0200

    Finishes tutorial

--- src/Players/Messages.elm
+++ src/Players/Messages.elm
@@ -7,3 +7,8 @@ import Players.Models exposing (PlayerId, Player)
 type Msg
   = FetchAllDone (List Player)
   | FetchAllFail Http.Error
+  | ShowPlayers
+  | ShowPlayer PlayerId
+  | ChangeLevel PlayerId Int
+  | SaveSuccess Player
+  | SaveFail Http.Error
--- src/Players/Update.elm
+++ src/Players/Update.elm
@@ -1,7 +1,9 @@
 module Players.Update exposing (..)
 
 import Players.Messages exposing (Msg(..))
-import Players.Models exposing (Player)
+import Players.Commands exposing (save)
+import Players.Models exposing (Player, PlayerId)
+import Navigation
 
 
 update : Msg -> List Player -> ( List Player, Cmd Msg )
@@ -11,3 +13,35 @@ update message players =
       ( newPlayers, Cmd.none )
     FetchAllFail error ->
       ( players, Cmd.none )
+    ShowPlayers ->
+      ( players, Navigation.newUrl "#players" )
+    ShowPlayer id ->
+      ( players, Navigation.newUrl ("#players/" ++ (toString id)) )
+    ChangeLevel id howMuch ->
+      ( players, changeLevelCommands id howMuch players |> Cmd.batch )
+    SaveSuccess updatedPlayer ->
+      ( updatePlayer updatedPlayer players, Cmd.none )
+    SaveFail error -> 
+      ( players, Cmd.none )
+
+changeLevelCommands : PlayerId -> Int -> List Player -> List (Cmd Msg)
+changeLevelCommands playerId howMuch = 
+  let 
+    cmdForPlayer existingPlayer = 
+      if existingPlayer.id == playerId then
+        save { existingPlayer | level = existingPlayer.level + howMuch }
+      else
+        Cmd.none
+  in
+    List.map cmdForPlayer
+
+updatePlayer : Player -> List Player -> List Player
+updatePlayer updatedPlayer = 
+  let
+    select existingPlayer = 
+      if existingPlayer.id == updatedPlayer.id then
+        updatedPlayer
+      else 
+        existingPlayer
+  in 
+    List.map select
